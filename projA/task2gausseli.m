329d20fa0bf103dc2e1e0ce28b673e4c5b3dd67f
Michał Szopiński <mszopinski@mssnt.pl>
2020-11-07 19:24:00 +0100

    Implement back-substitution

--- projA/task2gausseli.m
+++ projA/task2gausseli.m
@@ -5,9 +5,10 @@ for task = 'ab'
     b = genvector(task, 5);
     eqsys = [A, b];
     
-    disp(eqsys)
+    % perform gaussian elimination and back-substitution
     eqsys = gausseli(eqsys);
-    disp(eqsys)
+    eqsys = backsubst(eqsys);
+    disp([eqsys, A\b]);
 end
 
 % generates coefficient matrix for task A or B
@@ -55,6 +56,20 @@ function eqsys = gausseli(eqsys)
     for col = 1:size(eqsys, 1)
         for row = (col + 1):size(eqsys, 1)
             % calculate reductor constant and perform reduction
+            reductor = eqsys(row, col) / eqsys(col, col);
+            eqsys(row, :) = eqsys(row, :) - eqsys(col, :) * reductor;
+        end
+    end
+end
+
+% reduces triangular matrix to diagonal matrix
+function eqsys = backsubst(eqsys)
+    for col = size(eqsys, 1):-1:1
+        % normalize diagonal coefficients to 1
+        eqsys(col, :) = eqsys(col, :) / eqsys(col, col);
+        
+        % eliminate factor from other rows
+        for row = (col - 1):-1:1
             reductor = eqsys(row, col) / eqsys(col, col);
             eqsys(row, :) = eqsys(row, :) - eqsys(col, :) * reductor;
         end
