08e3ae4fe3683c6e5a9eb60be1955a40f2da12e4
Michał Szopiński <mszopinski@mssnt.pl>
2020-12-02 20:11:44 +0100

    Move rootbrac to separate file

--- projB/task1functroot.m
+++ projB/task1functroot.m
@@ -54,33 +54,6 @@ function y = taskfunc(x)
     y = 0.7 * x * cos(x) - log(x + 1);
 end
 
-% finds the root brackets of a function within the given range
-function brackets = rootbrac(func, rangestart, rangeend)
-    % define search resolution
-    resolution = (rangeend - rangestart) / 10;
-    
-    % start search at the start of the range
-    a = rangestart;
-    b = rangestart + resolution;
-    brackets = double.empty(2, 0);
-    
-    % keep moving the interval until the range is exceeded
-    while 1
-        % if the function changes sign inside the interval, a bracket has been found
-        if sign(func(a)) ~= sign(func(b))
-            % save bracket
-            brackets(:, size(brackets, 2) + 1) = [a, b];
-        end
-        
-        % if the bracket can't be expanded, return
-        if b == rangeend; return; end
-        
-        % check next bracket
-        a = b;
-        b = min(a + resolution, rangeend);
-    end
-end
-
 % uses the bisection algorithm to find the root of a function within the given bracket
 function [zero, steps] = bisect(func, a, b, tolerance)
     % initialize empty array of steps
