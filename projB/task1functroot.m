49b6228f4cff45be5d082c7b3c67f245aefa168d
Michał Szopiński <mszopinski@mssnt.pl>
2020-12-03 00:42:40 +0100

    Solve polynomial using Newton

--- projB/task1functroot.m
+++ projB/task1functroot.m
@@ -17,6 +17,13 @@ brackets = rootbrac(@taskfunc, interval(1), interval(2));
 printroots(@taskfunc, algorithms, interval, brackets, ...
     'Approximate zeros of function', 'zeros');
 
+% use Newton's algorithm to find roots of polynomial
+algorithms = {'Newton''s algorithm', @newton};
+interval = [1, 7];
+brackets = rootbrac(@polynomial, interval(1), interval(2));
+printroots(@polynomial, algorithms, interval, brackets, ...
+    'Approximate real roots of polynomial', 'realroots');
+    
 % the function as given in the task
 function y = taskfunc(x)
     y = 0.7 * x * cos(x) - log(x + 1);
@@ -34,7 +41,7 @@ function [zero, steps] = bisect(func, a, b, tolerance)
         steps(:, size(steps, 2) + 1) = [zero, func(zero)];
         
         % stop test
-        if abs(func(zero)) <= tolerance; break; end
+        if abs(a - b) < tolerance; break; end
         
         % choose next sub-interval based on sign mismatch
         if sign(func(a)) ~= sign(func(zero))
@@ -58,6 +65,7 @@ function [zero, steps] = newton(func, a, b, tolerance)
     % iterate algorithm until the error is within tolerance
     while 1
         % calculate next approximation of zero
+        prevzero = zero;
         derivative = (func(zero + step) - func(zero - step)) / (2 * step);
         zero = zero - func(zero) / derivative;
         steps(:, size(steps, 2) + 1) = [zero, func(zero)];
@@ -68,6 +76,6 @@ function [zero, steps] = newton(func, a, b, tolerance)
         end
         
         % stop test
-        if abs(func(zero)) <= tolerance; break; end
+        if abs(zero - prevzero) <= tolerance; break; end
     end
 end
