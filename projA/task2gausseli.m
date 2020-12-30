6ec65319dc93a1d34f1b85de2e21b675bdd1db8e
Michał Szopiński <mszopinski@mssnt.pl>
2020-12-30 19:43:14 +0100

    Move backsubst to separate file

--- projA/task2gausseli.m
+++ projA/task2gausseli.m
@@ -113,23 +113,6 @@ function [eqsys, lower, upper, permut] = gausseli(eqsys)
     upper = eqsys(:, 1:size(eqsys, 2) - 1);
 end
 
-% solves system with triangular matrix
-function result = backsubst(eqsys)
-    for col = size(eqsys, 1):-1:1
-        % normalize diagonal coefficients to 1
-        eqsys(col, :) = eqsys(col, :) / eqsys(col, col);
-        
-        % eliminate factor from other rows
-        for row = (col - 1):-1:1
-            reductor = eqsys(row, col) / eqsys(col, col);
-            eqsys(row, :) = eqsys(row, :) - eqsys(col, :) * reductor;
-        end
-    end
-    
-    % rightmost column is now the result
-    result = eqsys(:, size(eqsys, 2));
-end
-
 % performs back-substitution for lower triangular matrices
 function result = backsubstflip(eqsys)
     % flip the equation system for lower triangular matrices
