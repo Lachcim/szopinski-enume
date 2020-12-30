737e8aa7d84d37ddf6d4bd51468111c034c28f6b
Michał Szopiński <mszopinski@mssnt.pl>
2020-12-30 19:08:31 +0100

    Move QR decomp to separate file

--- projA/task4qreigen.m
+++ projA/task4qreigen.m
@@ -105,37 +105,3 @@ function eigen = eigenoftwo(A)
         eigen = eigen2;
     end
 end
-
-% performs QR decomposition of a matrix
-function [Q, R] = qrdecomp(A)
-    % initialize empty matrices
-    Q = zeros(size(A));
-    R = eye(size(A, 2));
-    
-    % modified Gram-Schmidt, use each column to orthogonalize the ones in front of it
-    for col = 1:size(A, 2)
-        % by the time we've reached this column, it's already been orthogonalized
-        Q(:, col) = A(:, col);
-        
-        % calculate current column dot product for R
-        coldot = dot(Q(:, col), Q(:, col));
-        
-        % orthogonalize further columns
-        for next = (col + 1):size(A, 2)
-            % calculate R cell for this column pair
-            R(col, next) = dot(Q(:, col), A(:, next)) / coldot;
-            
-            % orthogonalize column
-            A(:, next) = A(:, next) - R(col, next) * Q(:, col);
-        end
-    end
-    
-    % normalize matrix
-    normalizer = zeros(size(Q));
-    for col = 1:size(Q, 2)
-        colnorm = norm(Q(:, col));
-        normalizer(col, col) = colnorm;
-        Q(:, col) = Q(:, col) / colnorm;
-    end
-    R = normalizer * R;
-end
