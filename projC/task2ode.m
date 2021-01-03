62a5c5b94600e8cf44c9186222feb20be4fa3f04
Michał Szopiński <mszopinski@mssnt.pl>
2021-01-03 21:28:54 +0100

    Implement RK4

--- projC/task2ode.m
+++ projC/task2ode.m
@@ -8,21 +8,33 @@ sysfuncts = {
     @(x) x(2) + x(1) * (0.5 - x(1)^2 - x(2)^2);
     @(x) -x(1) + x(2) * (0.5 - x(1)^2 - x(2)^2)
 };
-initvalues = [0; 0];
+initvalues = [0; 12];
 
-eulerode(sysfuncts, initvalues, 0, 15, 1)
+rk4(sysfuncts, initvalues, 0, 15, 0.01)
 
-function x = eulerode(functs, init, a, b, stepsize)
+function x = rk4(functs, init, a, b, stepsize)
     % set initial values as start points of output
     x = init;
     
     % build output based on preceding values
     stepcount = ceil((b - a)/stepsize);
     for step = 1:stepcount
+        % obtain the preceding function values
+        stepval = x(:, step);
+        
         for eqnum = 1:size(functs, 1)
-            prevderiv = functs{eqnum}(x(:, step));
+            % obtain the function for this equation
+            fun = functs{eqnum};
             
-            x(eqnum, step + 1) = x(eqnum, step) + stepsize * prevderiv;
+            % calculate the phi for RK4
+            k1 = fun(stepval);
+            k2 = fun(stepval + 0.5 * stepsize * k1);
+            k3 = fun(stepval + 0.5 * stepsize * k2);
+            k4 = fun(stepval + stepsize * k3);
+            phi = (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+            
+            % generic single-step iteration
+            x(eqnum, step + 1) = x(eqnum, step) + stepsize * phi;
         end
     end
     
