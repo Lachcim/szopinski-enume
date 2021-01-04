cc65054b8f028612dba7142de60ec3a99ab281d3
Michał Szopiński <mszopinski@mssnt.pl>
2021-01-04 14:09:01 +0100

    Prepare RK4auto

--- projC/task2ode.m
+++ projC/task2ode.m
@@ -10,7 +10,7 @@ sysfuncts = {
 };
 initvalues = [0; 12];
 
-rk4(sysfuncts, initvalues, 0, 15, 0.013408);
+rk4auto(sysfuncts, initvalues, 0, 15, 0.013408);
 
 % solve ODE system using RK4 with constant step size
 function x = rk4(functs, init, a, b, stepsize)
@@ -32,6 +32,36 @@ function x = rk4(functs, init, a, b, stepsize)
     
     % append arguments to output
     x = [a:stepsize:(stepcount * stepsize); x];
+end
+
+% automatic step size variant of RK4
+function x = rk4auto(functs, init, a, b, initstep)
+    % set start points of output
+    args = a;
+    x = init;
+    
+    % integrate function until end of interval reached
+    stepsize = initstep;
+    step = 0;
+    while 1
+        % obtain the preceding function values
+        step = step + 1;
+        stepval = x(:, step);
+        
+        % advance output function
+        for eqnum = 1:size(functs, 1)
+            % generic single-step iteration
+            phi = rk4phi(functs{eqnum}, stepval, stepsize);
+            x(eqnum, step + 1) = x(eqnum, step) + stepsize * phi;
+            args(step + 1) = args(step) + stepsize;
+        end
+        
+        % stop algorithm if function integrated over the whole interval
+        if args(end) >= b; break; end
+    end
+    
+    % append arguments to output
+    x = [args; x];
 end
 
 % calculate the phi for RK4 algorithms
